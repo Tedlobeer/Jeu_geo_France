a926a3fb522d0fd4126e14c7c01af6468548b1cc
Teddy LORENTZ <teddylorentz@gmail.com>
2023-07-18 02:28:15 +0800

    Début du projet, ajout nav france/monde et ajouts de tous les liens nav sauf loc

---
+++ Jeu_geo_fr/jeu_geo.js
@@ -0,0 +1,21 @@
+let boutonNavFrance = document.getElementById("mode_france");
+let boutonNavMonde = document.getElementById("mode_monde");
+let navFrance = document.getElementById("nav_france");
+let navMonde = document.getElementById("nav_monde");
+
+boutonNavFrance.addEventListener("click", ()=>{
+    navFrance.classList.add("enable");
+    navMonde.classList.remove("enable");
+    navFrance.classList.remove("disable");
+    navMonde.classList.add("disable");
+});
+
+boutonNavMonde.addEventListener("click", ()=>{
+    navFrance.classList.remove("enable");
+    navMonde.classList.add("enable");
+    navFrance.classList.add("disable");
+    navMonde.classList.remove("disable");
+});
+
+//affichage du nombre de départements dans l'input range
+
