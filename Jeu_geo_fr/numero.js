eb29fd919c7dfa3d19019c249cd9f9d0f507fa11
Teddy LORENTZ <teddylorentz@gmail.com>
2023-07-22 17:33:38 +0800

    programmation de numero.js jusqu'au preLaunch : TOUT FONCTIONNE

--- Jeu_geo_fr/numero.js
+++ Jeu_geo_fr/numero.js
@@ -9,71 +9,77 @@ async function chargerDepartements(){
         console.error("Une erreur s'est produite lors du chargement du fichier JSON :", error);
     }
 }
-    // Récupération des éléments du DOM
-    const container = document.querySelector(".container");
+// Récupération des éléments du DOM
+const container = document.querySelector(".container");
 
     
 
-    //fonction qui va générer un nb aléatoire parmi tous les départements fr
+//fonction qui va générer un nb aléatoire parmi tous les départements fr
+function genererAlea(){
+    return Math.floor(Math.random() * (101)) + 1;
+} 
 
-    function genererAlea(){
-        return Math.floor(Math.random() * (101)) + 1;
-    } 
+// Fonction qui vérifie si le nombre généré n'existe pas déjà dans le tableau de stockage
 
-    // Fonction qui vérifie si le nombre généré n'existe pas déjà dans le tableau de stockage
-
-    function verifierAbs(nbAlea,index,tab_stockage){
-        let condition = true;
-        for (let i = 0; i <= index; i++) {
-            if (nbAlea == tab_stockage[i]) {
-                condition = false;
-            };
-        }
-        return condition;
+function verifierAbs(nbAlea,index,tab_stockage){
+    let condition = true;
+    for (let i = 0; i <= index; i++) {
+        if (nbAlea == tab_stockage[i]) {
+            condition = false;
+        };
     }
+    return condition;
+}
 
-    // Fonction qui génère le tableau contenant les indices aléatoires des départements à deviner
+// Fonction qui génère le tableau contenant les indices aléatoires des départements à deviner
 
-    function genererTabJeu(nbUser){
-        let tab_stockage = [];
-        for (let i = 0; i < nbUser; i++) {
-            let condition = false;
-            let index = i;
-            let nbAlea = 0;
-            while (condition == false) {
-                nbAlea = genererAlea();
-                condition = verifierAbs(nbAlea,index,tab_stockage);
-            }
-            tab_stockage[i] = nbAlea; 
+function genererTabJeu(nbUser){
+    let tab_stockage = [];
+    for (let i = 0; i < nbUser; i++) {
+        let condition = false;
+        let index = i;
+        let nbAlea = 0;
+        while (condition == false) {                
+            nbAlea = genererAlea();
+            condition = verifierAbs(nbAlea,index,tab_stockage);
         }
-        return tab_stockage;
+        tab_stockage[i] = nbAlea; 
     }
+    return tab_stockage;
+}
+
+//fonction qui affiche le nombre correspondant au curseur de la range
+function affichageCurseur(){
+    const curseur = document.getElementById("range_dep");
+    const valeurCurseur = document.getElementById("valeur_curseur");
+
+    curseur.addEventListener("input", ()=>{
+        valeurCurseur.textContent = curseur.value + " ";
+    });
+}
+
+//fonction au clic de valider masquage de la range, apparition du container questions et création du tableau contenant les index
+function preLaunch(){
+    const curseur = document.getElementById("range_dep");
+    const boutonValider = document.getElementById("valider");
+    const containerQuestions = document.getElementById("container_questions");
+
+    boutonValider.addEventListener("click", ()=>{
+        container.classList.add("disable");
+        containerQuestions.classList.remove("disable");
+        const nbDepUser = curseur.value;
+        let tab_index = genererTabJeu(nbDepUser);
+    });
+}
 
 // Fonction d'initialisation
 async function init() {
     // Attendre que les départements soient chargés avant de continuer
     await chargerDepartements();
-    
-    //affichage du nombre de départements dans l'input range
-    
-    const curseur = document.getElementById("range_dep");
-    const valeurCurseur = document.getElementById("valeur_curseur");
-    
-    curseur.addEventListener("input", ()=>{
-        valeurCurseur.textContent = curseur.value + " ";
-    });
-
-// Masquage de la range lorsque l'utilisateur clique sur "Valider"
-
-const boutonValider = document.getElementById("valider");
-boutonValider.addEventListener("click", ()=>{
-    container.classList.add("disable");
-
-    // Génération du tableau et affichage dans la console
-    const nbDepUser = curseur.value;
-    let test = genererTabJeu(nbDepUser);
-    console.log(test);
-});
+    //appel de la fonction d'affichage du nombre lié au curseur
+    affichageCurseur();
+    //appel de la fonction de préparation du quizz
+    preLaunch();
 }
 
 init();
